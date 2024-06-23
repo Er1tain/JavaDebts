ff65d76877a6f279281e37e9c4a4b98d564750a0
unknown <sanyapridava@mail.ru>
2024-06-24 00:38:51 +0300

    Running methods

--- Collections/src/Main.java
+++ Collections/src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
+import CountLittersText.AnalysText;
+
+import java.io.FileInputStream;
+import java.util.HashSet;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        //Analysing text info
+        AnalysText.countLitters("C:\\JavaDebts\\Collections\\src\\CountLittersText\\Text.txt");
+        AnalysText.mostFrequentLitters("C:\\JavaDebts\\Collections\\src\\CountLittersText\\Text.txt");
 
     }
 }
